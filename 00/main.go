38443a1c6cc1770464726c0fe660c78c33c9a118
Alberto Bregliano <alberto.bregliano@pm.me>
2020-10-21 18:05:42 +0200

    ok

--- 00/main.go
+++ 00/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"strings"
 
 	"github.com/fatih/color"
 	"github.com/howeyc/gopass"
@@ -50,60 +47,6 @@ func main() {
 		fmt.Printf("ex04 %s %s\n", red("KO"), err.Error())
 	}
 
-}
-
-func ex00() (bool, error) {
-	dir := "ex00"
-	file := "z"
-	_, err := controllofile(1, file, dir)
-	if err != nil {
-		return false, err
-	}
-	catCmd := exec.Command("bash", "-c", "cat ex00/z")
-	catOut, err := catCmd.Output()
-	if err != nil {
-		return false, err
-	}
-	if string(catOut) != "Z" {
-		return false, errors.New("contenuto file z non corrisponde")
-	}
-	return true, nil
-}
-
-func ex01() (bool, error) {
-	dir := "ex01"
-	file := "testShell00.tar"
-	_, err := controllofile(1, file, dir)
-	if err != nil {
-		return false, err
-	}
-
-	catCmd := exec.Command("bash", "-c", "tar -xvf testShell00.tar")
-	catCmd.Dir = dir
-
-	_, err = catCmd.Output()
-	if err != nil {
-		return false, err
-	}
-	// rimuove file untarred quando ha finito.
-	defer os.Remove(dir + "/" + "testShell00")
-
-	lsCMD := exec.Command("bash", "-c", "ls -l testShell00")
-	lsCMD.Dir = dir
-	lsOut, err := lsCMD.Output()
-	output := string(lsOut)
-	fields := strings.Split(output, " ")
-	fields[2] = "XX"
-	fields[3] = "XX"
-	output = strings.Join(fields, " ")
-
-	outputatteso := "-r--r-xr-x. 1 XX XX 40 Jun  1 23:42 testShell00"
-
-	if !strings.Contains(output, outputatteso) {
-		return false, fmt.Errorf("output non corretto\n%s%s", output, outputatteso)
-	}
-
-	return true, nil
 }
 
 func controllofile(numfiles int, file, dir string) (string, error) {
