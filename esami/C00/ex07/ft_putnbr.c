51404082c6fb61d1e9952eaf88d0407dd5eb3a9c
Alberto Bregliano <alberto.bregliano@pm.me>
2020-10-25 17:16:58 +0100

    ok

--- esami/C00/ex07/ft_putnbr.c
+++ esami/C00/ex07/ft_putnbr.c
@@ -6,15 +6,34 @@
 /*   By: abreglia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 11:04:18 by abreglia          #+#    #+#             */
-/*   Updated: 2020/10/25 12:45:58 by abreglia         ###   ########.fr       */
+/*   Updated: 2020/10/25 17:15:30 by abreglia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_putnbr(int nb)
 {
-	char c[sizeof(nb)];
-	write(1, &c,1);
+	char arr[100];
+
+	char a;
+
+	int o;
+
+	int size;
+
+	size = 0;
+
+	while(nb != 0)
+	{
+		o = nb % 10;
+		arr[size] = o + '0';
+		size++;
+		nb = nb / 10;
+	}
+	while(size >=0)
+	{	a = arr[size];
+		write(1, &a, 1);
+		size--;
+	}
 }
